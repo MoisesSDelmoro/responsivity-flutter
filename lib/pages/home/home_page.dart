c08681c04a8b005431b4bfba7d78ff559dd432d3
MoisesSDelmoro <moisesdelmoro@gec.inatel.br>
2022-07-26 14:35:49 -0300

    top section finished

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -12,6 +12,7 @@ class HomePage extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
+          backgroundColor: Colors.black,
           drawer: constraints.maxWidth < flutterAppMobileBreakpoints
               ? const Drawer()
               : null,
--- lib/pages/home/widgets/section/top_section.dart
+++ lib/pages/home/widgets/section/top_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsividadeflutter/constants/constants.dart';
 import 'package:responsividadeflutter/pages/home/widgets/section/custom_search_field.dart';
 
 class TopSection extends StatelessWidget {
@@ -32,7 +33,7 @@ class TopSection extends StatelessWidget {
                       child: Column(
                         children: const [
                           Text(
-                            "Desenvolvendo Flutter neste curso",
+                            "Desenvolver é a melhor forma de aprender",
                             style: TextStyle(
                               fontSize: 36,
                               fontWeight: FontWeight.bold,
@@ -62,7 +63,102 @@ class TopSection extends StatelessWidget {
             ),
           );
         }
-        return Container();
+        if (constraints.maxWidth >= flutterAppMobileBreakpoints) {
+          return SizedBox(
+            height: 320,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.asset(
+                    "images/udemy.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  top: 20,
+                  left: 20,
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 8,
+                    child: Container(
+                      padding: const EdgeInsets.all(22),
+                      width: 350,
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Desenvolver é a melhor forma de aprender",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Projeto desenvolvido para aprender/adquirir/melhorar/compreender novos conceitos e dinâmica de responsidade dos widgets-componentes do Flutter",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 14,
+                          ),
+                          CustomSearchField(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        return Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 3.4,
+              child: Image.asset(
+                "images/udemy.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Desenvolver é a melhor forma de aprender",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "Projeto desenvolvido para aprender/adquirir/melhorar/compreender novos conceitos e dinâmica de responsidade dos widgets-componentes do Flutter",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  CustomSearchField(),
+                ],
+              ),
+            )
+          ],
+        );
       },
     );
   }
